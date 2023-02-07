be07cf42a90025d4d4e2f67c3b012d025bb22dc3
Muhammad Faysal <srfahim23@gmail.com>
2023-02-07 16:59:30 +0600

    this-update

---
+++ main.py
@@ -0,0 +1,14 @@
+# dir-dict-the help methods
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age 
+        self.version = 1
+
+
+p = Person("John", 30)    
+print(p.__dict__)
+
+print(help(Person))
+
